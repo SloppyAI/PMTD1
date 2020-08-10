c2393ea1c1f5abbef91065578a1abeaf7d60da1c
Admin <Admin@192.168.1.7>
2020-08-10 15:34:37 +0700

    Thêm chức năng sắp xếp

---
+++ src/Database/DAL.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Admin
+ */
+public class DAL {
+
+    public static ResultSet GetAllCongViec() {
+        String sql = "select TENCTY, TENCV, DIADIEM, Luong, NgayDang  "
+                + "from CONGTY inner join CONGVIEC "
+                + "on CONGTY.ID_CONGTY = CONGVIEC.ID_CONGTY";
+        return DBConnection.GetData(sql);
+    }
+
+    //Tạo hàm select dữ liệu sắp xếp từ Database
+    
+    //Sắp xếp theo ngày đăng mới nhất
+    public static ResultSet SXBaiDangMoiNhat() {
+        String sql = " select TENCTY, TENCV, DIADIEM, Luong, NgayDang from CONGTY inner join CONGVIEC "
+                + "on CONGTY.ID_CONGTY = CONGVIEC.IDCV\n"
+                + "order by NgayDang Desc";
+        return DBConnection.GetData(sql);
+    }
+    
+    //Sắp xếp theo ngày đăng cũ nhất
+    public static ResultSet SXBaiDangCuNhat() {
+        String sql = " select TENCTY, TENCV, DIADIEM, Luong, NgayDang from CONGTY inner join CONGVIEC "
+                + "on CONGTY.ID_CONGTY = CONGVIEC.IDCV\n"
+                + "order by NgayDang Asc";
+        return DBConnection.GetData(sql);
+    }
+    
+    //Sắp xếp theo lương từ cao đến thấp
+    public static ResultSet SXBaiDangLuongDESC() {
+        String sql = " select TENCTY, TENCV, DIADIEM, Luong, NgayDang from CONGTY inner join CONGVIEC "
+                + "on CONGTY.ID_CONGTY = CONGVIEC.IDCV\n"
+                + "order by Luong Desc";
+        return DBConnection.GetData(sql);
+    }
+    
+    //Sắp xếp theo lương từ thấp đến cao
+    public static ResultSet SXBaiDangLuongASC() {
+        String sql = " select TENCTY, TENCV, DIADIEM, Luong, NgayDang from CONGTY inner join CONGVIEC "
+                + "on CONGTY.ID_CONGTY = CONGVIEC.IDCV\n"
+                + "order by Luong Asc";
+        return DBConnection.GetData(sql);
+    }
+
+
+}
---
+++ src/Database/DBConnection.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class DBConnection {
+    private static String DB_URL = "jdbc:sqlserver://localhost;"
+            + "databaseName=QL_TuyenDung;";
+    private static String USER_NAME = "sa";
+    private static String PASSWORD = "seeker305";    
+    static Connection conn = null;
+
+    public static void ConnectSql() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("connect successfully!");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Lỗi 01 " );
+        } catch (SQLException ex) {
+            System.out.println("Lỗi kết nối SQL");
+        }
+    }
+    
+    //Hàm thực hiện câu lệnh select
+    public static ResultSet GetData(String cauTruyVan){
+        try {
+            ConnectSql();
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(cauTruyVan);
+            
+            return rs;
+           
+        } catch (SQLException ex) {
+            System.out.println("lỗi lấy dữ liệu");
+            return null;
+        }
+    }
+    //Hàm thực hiện 3 lệnh insert update delete
+    public static int ExecuteQueryUpdate(String sql){
+        try {
+             ConnectSql();
+        // tạo 1 statement
+        Statement stm = conn.createStatement();
+       
+        return stm.executeUpdate(sql);
+        } catch (SQLException ex) {
+        System.out.println("lỗi sql." + ex.getMessage());
+        return -1 ;
+    }
+    
+   }  
+
+
+
+
+}
